58a1a0a961aea6739df6ccdec24fe694510e2ab8
Christine Lau <clau@stitchfix.com>
2017-08-11 16:35:18 -0700

    added the end of the year code for the dummy fiscal year cal

--- lib/merch_calendar/fy_cal.rb
+++ lib/merch_calendar/fy_cal.rb
@@ -1,28 +1,14 @@
 require "merch_calendar/retail_calendar"
 require "date"
 
-
-
 module MerchCalendar
   class FyCal
     
-    def weeks_in_year
+    def initialize
       
     end
     
-    def start_of_week
-      
-    end
-    
-    def end_of_week
-      
-    end
-    
-    def start_of_month
-      
-    end
-    
-    def end_of_month
+    def start_of_year
       
     end
     
@@ -33,15 +19,38 @@ module MerchCalendar
     def end_of_quarter
       
     end
+
+    def end_of_year(year)
+      year_end = Date.new((year), 7, -1) # July 31st
+      wday = (year_end.wday + 1) % 7 # 5
+
+      if wday > 3 ### this rounds up to the next saturday 
+        year_end += 7 - wday
+      else# rounding down to the next saturday
+        year_end -= wday
+      end
+      year_end
+    end
     
-    def start_of_year
+    def start_of_month
       
     end
     
-    def end_of_year
+    def end_of_month
+      
+    end
+
+    def start_of_week
       
     end
     
+    def end_of_week
+      
+    end
     
+    def weeks_in_year
+      
+    end
+
   end
 end
