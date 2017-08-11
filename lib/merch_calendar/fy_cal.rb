3836486500c738852ffcaec270fd087ce511218c
Christine Lau <clau@stitchfix.com>
2017-08-11 16:25:44 -0700

    added new methods to the fy

--- lib/merch_calendar/fy_cal.rb
+++ lib/merch_calendar/fy_cal.rb
@@ -6,7 +6,41 @@ require "date"
 module MerchCalendar
   class FyCal
     
+    def weeks_in_year
+      
+    end
     
+    def start_of_week
+      
+    end
+    
+    def end_of_week
+      
+    end
+    
+    def start_of_month
+      
+    end
+    
+    def end_of_month
+      
+    end
+    
+    def start_of_quarter
+      
+    end
+    
+    def end_of_quarter
+      
+    end
+    
+    def start_of_year
+      
+    end
+    
+    def end_of_year
+      
+    end
     
     
   end
